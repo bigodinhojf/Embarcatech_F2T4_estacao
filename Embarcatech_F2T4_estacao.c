5f9585fd9a2aa60361b111b2a2b5dd1ca8596dd9
bigodinhojf <jfelipe.teles1@gmail.com>
2025-05-18 12:32:40 -0300

    first commit

---
+++ Embarcatech_F2T4_estacao.c
@@ -0,0 +1,239 @@
+// -- Inclusão de bibliotecas
+#include "pico/stdlib.h"
+#include "hardware/gpio.h"
+#include "hardware/adc.h"
+#include "hardware/i2c.h"
+#include "hardware/pwm.h"
+#include "hardware/pio.h"
+#include "ws2812.pio.h"
+#include "lib/ssd1306.h"
+#include "lib/font.h"
+#include "FreeRTOS.h"
+#include "FreeRTOSConfig.h"
+#include "task.h"
+#include "queue.h"
+#include <stdio.h>
+#include "pico/bootrom.h"
+
+// -- Definição de constantes
+// GPIO
+#define button_B 6 // Botão B GPIO 6
+#define matriz_leds 7 // Matriz de LEDs GPIO 7
+#define NUM_LEDS 25 // Número de LEDs na matriz
+#define buzzer_A 21 // Buzzer A GPIO 21
+#define buzzer_B 10 // Buzzer B GPIO 10
+#define LED_Green 11 // LED Verde GPIO 11
+#define LED_Red 13 // LED Vermelho GPIO 13
+#define joystick_Y 26 // VRY do Joystick GPIO 26
+#define joystick_X 27 // VRX do Joystick GPIO 27
+
+// Display I2C
+#define display_i2c_port i2c1 // Define a porta I2C
+#define display_i2c_sda 14 // Define o pino SDA na GPIO 14
+#define display_i2c_scl 15 // Define o pino SCL na GPIO 15
+#define display_i2c_endereco 0x3C // Define o endereço do I2C
+ssd1306_t ssd; // Inicializa a estrutura do display
+
+typedef struct{
+    float volume_chuva;
+    float nivel_agua;
+} VolumeNivel_data_t;
+
+QueueHandle_t xQueueEstacaoModo; // Fila para guardar modo
+QueueHandle_t xQueueVolumeNivel; // Fila para guardar o volume e o nivel
+
+// --- Funções necessária para a manipulação da matriz de LEDs
+
+// Estrutura do pixel GRB (Padrão do WS2812)
+struct pixel_t {
+    uint8_t G, R, B; // Define variáveis de 8-bits (0 a 255) para armazenar a cor
+};
+typedef struct pixel_t pixel_t;
+typedef pixel_t npLED_t; // Permite declarar variáveis utilizando apenas "npLED_t"
+
+// Declaração da Array que representa a matriz de LEDs
+npLED_t leds[NUM_LEDS];
+
+// Variáveis para máquina PIO
+PIO np_pio;
+uint sm;
+
+// Função para definir a cor de um LED específico
+void cor(const uint index, const uint8_t r, const uint8_t g, const uint8_t b) {
+    leds[index].R = r;
+    leds[index].G = g;
+    leds[index].B = b;
+}
+
+// Função para desligar todos os LEDs
+void desliga() {
+    for (uint i = 0; i < NUM_LEDS; ++i) {
+        cor(i, 0, 0, 0);
+    }
+}
+
+// Função para enviar o estado atual dos LEDs ao hardware  - buffer de saída
+void buffer() {
+    for (uint i = 0; i < NUM_LEDS; ++i) {
+        pio_sm_put_blocking(np_pio, sm, leds[i].G);
+        pio_sm_put_blocking(np_pio, sm, leds[i].R);
+        pio_sm_put_blocking(np_pio, sm, leds[i].B);
+    }
+}
+
+// Função para converter a posição da matriz para uma posição do vetor.
+int getIndex(int x, int y) {
+    // Se a linha for par (0, 2, 4), percorremos da esquerda para a direita.
+    // Se a linha for ímpar (1, 3), percorremos da direita para a esquerda.
+    if (y % 2 == 0) {
+        return 24-(y * 5 + x); // Linha par (esquerda para direita).
+    } else {
+        return 24-(y * 5 + (4 - x)); // Linha ímpar (direita para esquerda).
+    }
+}
+
+// --- Final das funções necessária para a manipulação da matriz de LEDs
+
+void vJoystickTask(void *params){
+    adc_init();
+    adc_gpio_init(joystick_X);
+    adc_gpio_init(joystick_Y);
+
+    bool modo_alerta = false;
+    VolumeNivel_data_t VolNiv;
+
+    while (true){
+        adc_select_input(0); // Seleciona o ADC0 referente ao VRY do Joystick (GPIO 26)
+        uint16_t value_vry = adc_read(); // Ler o valor do ADC selecionado (ADC0 - VRY) e guarda
+        adc_select_input(1); // Seleciona o ADC1 referente ao VRX do Joystick (GPIO 27)
+        uint16_t value_vrx = adc_read(); // Ler o valor do ADC selecionado (ADC1 - VRX) e guarda
+
+        VolNiv.volume_chuva = (value_vry/4095.0) * 100.0;
+        VolNiv.nivel_agua = (value_vrx/4095.0) * 100.0;
+
+        if((VolNiv.volume_chuva >= 80) || (VolNiv.nivel_agua >= 70)){
+            modo_alerta = true;
+        }else{
+            modo_alerta = false;
+        }
+        printf("Volume de chuva: %.1f | Nivel da agua: %.1f | Modo alerta: %d\n", VolNiv.volume_chuva, VolNiv.nivel_agua, modo_alerta);
+        xQueueSend(xQueueEstacaoModo, &modo_alerta, 0); // Envia o valor para fila
+        xQueueSend(xQueueVolumeNivel, &VolNiv, 0); // Envia o valor para fila
+        vTaskDelay(pdMS_TO_TICKS(100));
+    }
+}
+
+void vDisplayTask(void *params){
+    // Inicialização dp Display I2C
+    i2c_init(display_i2c_port, 400 * 1000); // Inicializa o I2C usando 400kHz
+    gpio_set_function(display_i2c_sda, GPIO_FUNC_I2C); // Define o pino SDA (GPIO 14) na função I2C
+    gpio_set_function(display_i2c_scl, GPIO_FUNC_I2C); // Define o pino SCL (GPIO 15) na função I2C
+    gpio_pull_up(display_i2c_sda); // Ativa o resistor de pull up para o pino SDA (GPIO 14)
+    gpio_pull_up(display_i2c_scl); // Ativa o resistor de pull up para o pino SCL (GPIO 15)
+    ssd1306_init(&ssd, WIDTH, HEIGHT, false, display_i2c_endereco, display_i2c_port); // Inicializa o display
+    ssd1306_config(&ssd); // Configura o display
+    ssd1306_send_data(&ssd); // Envia os dados para o display
+    ssd1306_fill(&ssd, false); // Limpa o display
+    ssd1306_send_data(&ssd); // Atualiza o display
+
+    bool modo_alerta;
+    VolumeNivel_data_t VolNiv;
+
+    char str_vol[3];
+    char str_niv[3];
+
+    while(true){
+        ssd1306_rect(&ssd, 0, 0, 127, 63, true, false); // Borda principal
+        ssd1306_line(&ssd, 1, 12, 126, 12, true); // Desenha uma linha horizontal
+        ssd1306_line(&ssd, 1, 24, 126, 24, true); // Desenha uma linha horizontal
+
+        // Cabeçalho
+        ssd1306_draw_string(&ssd, "EMB Semaforo", 16, 3); // Desenha uma string
+        ssd1306_draw_string(&ssd, "Modo:", 12, 15); // Desenha uma string
+
+        if(xQueueReceive(xQueueEstacaoModo, &modo_alerta, portMAX_DELAY)){
+            if(modo_alerta){
+                ssd1306_draw_string(&ssd, "Alerta", 60, 15); // Desenha uma string
+                ssd1306_draw_string(&ssd, "ABRIGUE-SE", 22, 51); // Desenha uma string
+            }else{
+                ssd1306_draw_string(&ssd, "Normal", 60, 15); // Desenha uma string
+                ssd1306_draw_string(&ssd, "          ", 22, 51); // Desenha uma string
+            }
+        }
+        ssd1306_draw_string(&ssd, "Vol. Chuva:   %", 4, 27); // Desenha uma string
+        ssd1306_draw_string(&ssd, "Niv.  Agua:   %", 4, 38); // Desenha uma string
+
+        if(xQueueReceive(xQueueVolumeNivel, &VolNiv, portMAX_DELAY)){
+            sprintf(str_vol, "%.0f", VolNiv.volume_chuva); // Converte float em string
+            sprintf(str_niv, "%.0f", VolNiv.nivel_agua); // Converte float em string
+            if(VolNiv.volume_chuva > 99.5){
+                ssd1306_draw_string(&ssd, str_vol, 92, 27); // Desenha uma string
+            }else if(VolNiv.volume_chuva > 9.9){
+                ssd1306_draw_string(&ssd, str_vol, 100, 27); // Desenha uma string
+            }else{
+                ssd1306_draw_string(&ssd, str_vol, 108, 27); // Desenha uma string
+            }
+            if(VolNiv.nivel_agua > 99.5){
+                ssd1306_draw_string(&ssd, str_niv, 92, 38); // Desenha uma string
+            }else if(VolNiv.nivel_agua > 9.9){
+                ssd1306_draw_string(&ssd, str_niv, 100, 38); // Desenha uma string
+            }else{
+                ssd1306_draw_string(&ssd, str_niv, 108, 38); // Desenha uma string
+            }
+        }
+
+        ssd1306_send_data(&ssd); // Atualiza o display
+    }
+}
+
+void vLEDTask(void *params){
+    gpio_init(LED_Green); // Inicia a GPIO 11 do LED Verde
+    gpio_set_dir(LED_Green, GPIO_OUT); // Define a direção da GPIO 11 do LED Verde como saída
+    gpio_put(LED_Green, false); // Estado inicial do LED apagado
+    gpio_init(LED_Red); // Inicia a GPIO 13 do LED Vermelho
+    gpio_set_dir(LED_Red, GPIO_OUT); // Define a direção da GPIO 13 do LED Vermelho como saída
+    gpio_put(LED_Red, false); // Estado inicial do LED apagado
+
+    bool modo_alerta;
+
+    while(true){
+        if(xQueueReceive(xQueueEstacaoModo, &modo_alerta, portMAX_DELAY)){
+            if(modo_alerta){
+                gpio_put(LED_Green, false);
+                gpio_put(LED_Red, true);
+            }else{
+                gpio_put(LED_Green, true);
+                gpio_put(LED_Red, false);
+            }
+        }
+    }
+}
+
+
+void gpio_irq_handler(uint gpio, uint32_t events){
+    reset_usb_boot(0, 0);
+}
+
+int main()
+{
+    // Ativa BOOTSEL via botão
+    gpio_init(button_B);
+    gpio_set_dir(button_B, GPIO_IN);
+    gpio_pull_up(button_B);
+    gpio_set_irq_enabled_with_callback(button_B, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler);
+
+    stdio_init_all();
+    sleep_ms(1000);
+
+    // Cria a fila para compartilhamento de valor do joystick
+    xQueueEstacaoModo = xQueueCreate(5, sizeof(bool));
+    xQueueVolumeNivel = xQueueCreate(5, sizeof(VolumeNivel_data_t));
+
+    // Criação das tasks
+    xTaskCreate(vJoystickTask, "Joystick", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
+    xTaskCreate(vDisplayTask, "Display", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
+    xTaskCreate(vLEDTask, "LED RGB", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
+    // Inicia o agendador
+    vTaskStartScheduler();
+    panic_unsupported();
+}
